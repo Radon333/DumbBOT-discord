c3898e6cc46f0cc6c4b5fa2bcac4d5e533417157
Ranveer Shah <ranveer2001s@gmail.com>
2021-01-24 12:33:49 +0530

    v0.2
    Added random replies to when someone messages "dumb_words"

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+import discord
+import os
+import random
+
 client=discord.Client()
+
+dumb_words=["dumb","stupid","idiot","mad","lunatic","pathetic","shit","unhappy","sad","fail"]
+
+dumb_replies=["Cheer Up! I am a result of a failed code,still working fine :)","The only stupid thing here is me","Hi i am DumbBOT, a bot who is ...... yeah you guessed it right.","I am idiot to join this server"]
 
 @client.event
 async def on_ready():
@@ -9,7 +17,11 @@ async def on_message(message):
   if message.author==client.user:
     return
 
-  if message.content.startswith("hi"):
-    await message.channel.send('Hi! i am created by Ranveer Shah for testing purpose')
+  msg=message.content
+
+  if msg.startswith("hi"):
+    await message.channel.send('Hi! i am DumbBOT created by Ranveer for testing purpose')
+  if any(word in msg for word in dumb_words):
+    await message.channel.send(random.choice(dumb_replies))
 
 client.run(os.getenv('TOKEN'))
