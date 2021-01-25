e94ee1aa276fcacf54ada35cb80d3c8088cf81a0
Ranveer Shah <ranveer2001s@gmail.com>
2021-01-25 20:09:27 +0530

    v0.5
    Added new commands to add new reply delete them and display them.The new replies are stored in a database

--- main.py
+++ main.py
@@ -1,12 +1,33 @@
 import discord
 import os
+import requests
+import json
 import random
+from replit import db
+from keep_alive import keep_alive
 
-client=discord.Client()
+client = discord.Client()
 
-dumb_words=["dumb","stupid","idiot","mad","lunatic","pathetic","shit","unhappy","sad","fail"]
+dumb_words = ["dumb","stupid","idiot","mad","lunatic","pathetic","shit","unhappy","sad","fail"]
 
-dumb_replies=["Cheer Up! I am a result of a failed code,still working fine :)","The only stupid thing here is me","Hi i am DumbBOT, a bot who is ...... yeah you guessed it right.","I am idiot to join this server"]
+dumb_replies = ["Cheer Up! I am a result of a failed code,still working fine :)","The only stupid thing here is me","Hi i am DumbBOT, a bot who is ...... yeah you guessed it right.","I am idiot to join this server"]
+
+if "responding" not in db.keys():
+  db["responding"] = True
+
+def update_replies(new_replies):
+  if "dumb" in db.keys():
+    dumb = db["dumb"]
+    dumb.append(new_replies)
+    db["dumb"] = dumb
+  else:
+    db["dumb"] = [new_replies]
+
+def delete_replies(index):
+  dumb = db["dumb"]
+  if len(dumb) > index:
+    del dumb[index]
+    db["dumb"] = dumb
 
 @client.event
 async def on_ready():
@@ -14,14 +35,51 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-  if message.author==client.user:
+  if message.author == client.user:
     return
 
-  msg=message.content
+  msg = message.content
 
+
+  if db["responding"]:
+    options = dumb_replies
+    if "dumb" in db.keys():
+      options = options + db["dumb"]
+
+    if any(word in msg for word in dumb_words):
+      await message.channel.send(random.choice(options))
+
+  if msg.startswith("$newreply"):
+    new_replies = msg.split("$newreply ",1)[1]
+    update_replies(new_replies)
+    await message.channel.send("dumb reply added successfully")
+    
   if msg.startswith("hi"):
     await message.channel.send('Hi! i am DumbBOT created by Ranveer for testing purpose')
-  if any(word in msg for word in dumb_words):
-    await message.channel.send(random.choice(dumb_replies))
 
+  if msg.startswith("$delreply"):
+    dumb = []
+    if "dumb" in db.keys():
+      index = int(msg.split("$delreply",1)[1])
+      delete_replies(index)
+      dumb = db["dumb"]
+    await message.channel.send(dumb)
+
+  if msg.startswith("$list"):
+    dumb = []
+    if "dumb" in db.keys():
+      dumb = db["dumb"]
+    await message.channel.send(dumb)
+
+  if msg.startswith("$responding"):
+    value = msg.split("$responding ",1)[1]
+
+    if value.lower() == "true":
+      db["responding"] = True
+      await message.channel.send("Responding is on.")
+    else:
+      db["responding"] = False
+      await message.channel.send("Responding is off.")
+
+keep_alive()
 client.run(os.getenv('TOKEN'))
